ce74003eaf5f6dec070ba70aa2c37caf586c7ea1
xinerki <simasadves@gmail.com>
2023-12-28 21:50:48 +0100

    fxmanifest time https://b.catgirlsare.sexy/pMefdOg6s0Xr.png

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'InversePower-FiveM'
+description 'InversePower Port to FiveM ( and in Lua )'
+author 'Blumlaut'
+author 'ikt32'
+
+client_script "InversePower.lua"
+server_script "ServerStuff.lua"
